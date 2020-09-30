69ba5e158b92eae44c3a0bd2ab76ac052e97b3b9
Sinjebos <Nicke_95_@hotmail.com>
2020-09-30 14:20:53 +0200

    message to jonny

--- Project.java
+++ Project.java
@@ -3,5 +3,6 @@ public class Project{
          
         System.out.println("Hello world");
          System.out.println("fungerar det ??"); //jonny
+         System.out.println("det fungerar ungefär såhär"); // Niklas
      }
 }
