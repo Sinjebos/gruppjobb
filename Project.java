03c575aa4dc8161c7f0b87c6d959859498e2960a
Sinjebos <Nicke_95_@hotmail.com>
2020-09-30 14:30:02 +0200

    you need a commit message

--- Project.java
+++ Project.java
@@ -4,5 +4,6 @@ public class Project{
         System.out.println("Hello world");
          System.out.println("fungerar det ??"); //jonny
          System.out.println("det fungerar ungefär såhär"); // Niklas
+         System.out.println("Test");
      }
 }
